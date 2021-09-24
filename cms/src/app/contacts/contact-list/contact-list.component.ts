e6b00fa141f7fbfaf06d6fb6b21590320ae55909
Tincho228 <martin_quintero521@hotmail.com>
2021-09-24 14:47:57 -0300

    contact-list creation

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Contact } from '../contact.model'
 @Component({
   selector: 'cms-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-
-  constructor() { }
+  contacts : Contact
+  
+  constructor() {
+    new Contact("1","R. Kent Jackson","jacksonk@byui.edu","208-496-3771","../../assets/images/jacksonk.jpg", null),
+    new Contact("2","R. Rex Barzee","barzeer@byui.edu","208-496-3768","../../assets/images/barzeer.jpg", null)
+   }
 
   ngOnInit(): void {
   }
