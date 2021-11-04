30d2111d70b6e8f33284d2067b5cbb6fbb99dce2
Tincho228 <martin_quintero521@hotmail.com>
2021-11-04 13:10:22 -0300

    forms, document edit component

--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
-
-  constructor() { }
+  originalDocument:Document;
+  document:Document;
+  editMode:boolean = false;
+  id:number;
+  constructor(
+    private documentService:DocumentService, 
+    private router:Router, 
+    private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params:Params)=>{
+      this.id = +params["id"];
+      if(this.id === null){
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocument(this.id);
+      if(this.originalDocument === null){
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    })  
   }
-
+  onSubmit(form:NgForm){
+    const value = form.value;
+    var newDocument = new Document(this.id, value.name, value.description,value.url,[]) 
+    
+  }
 }
