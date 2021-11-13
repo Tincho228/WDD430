0d96dc261a2207910c977f687dd697a80057915a
Tincho228 <martin_quintero521@hotmail.com>
2021-11-13 10:54:14 -0300

    adding headers

--- cms/src/app/documents/document.service.ts
+++ cms/src/app/documents/document.service.ts
@@ -2,6 +2,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Document } from './document.model';
 import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,43 @@ export class DocumentService {
   documentListChangedEvent = new Subject<Document[]>();
   documents:Document[]=[];
   maxDocumentId:number;
-  constructor() {
-    this.documents = MOCKDOCUMENTS;
+  constructor(private http:HttpClient) {
+    //this.documents = MOCKDOCUMENTS;
     this.maxDocumentId = this.getMaxId();
   }
 
   getDocuments():Document[]{
+   this.http
+   .get('https://angularproject-d66ee-default-rtdb.firebaseio.com/documents.json')
+   .subscribe(
+      // success method
+      (documents: Document[] ) => {
+          this.documents = documents;
+          this.maxDocumentId = this.getMaxId();
+          // sort the array
+          documents.sort(compare)
+          function compare(a:Document, b:Document) {
+            if (a.name < b.name) {
+              return -1;
+            }
+            if (a.name < b.name) {
+              return 1;
+            }
+            // a must be equal to b
+            return 0;
+          }
+          // emit the next document list change event
+          this.documentListChangedEvent.next(this.documents.slice()); 
+      },
+      // error method
+      (error: any) => {
+          console.log(error.message);
+      } 
+     
+   );
+   
     return this.documents.slice();
+   
   }
 
   getDocument(id:number){
@@ -73,5 +104,12 @@ export class DocumentService {
     const documentsListClone = this.documents.slice();
     this.documentListChangedEvent.next(documentsListClone);  
   }
-
+  storeDocuments(){
+    const headers = new HttpHeaders({'Content-type':'application/json'})
+    const convertedDocuments = JSON.stringify(this.documents);
+    this.http.put('https://angularproject-d66ee-default-rtdb.firebaseio.com/documents.json', convertedDocuments),
+    {
+      headers: headers
+    }
+  }
 }
