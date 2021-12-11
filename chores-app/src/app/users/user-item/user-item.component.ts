c839bb3a65e3dfff1d492bd963366d3515dc1c6e
Tincho228 <martin_quintero521@hotmail.com>
2021-12-11 15:26:16 -0300

    adding info item html

--- chores-app/src/app/users/user-item/user-item.component.ts
+++ chores-app/src/app/users/user-item/user-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Todo } from 'src/app/todos/todo.model';
 import { TodosService } from 'src/app/todos/todos.service';
 import { User } from '../user.model';
 
@@ -10,11 +12,19 @@ import { User } from '../user.model';
 export class UserItemComponent implements OnInit {
   @Input()  user:User
   myBudget:number = 0
+  todos:Todo[] = []
+  subscription:Subscription
   constructor(
     private todoService:TodosService
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.todos = await this.todoService.waitedTodos();
+    this.subscription = this.todoService.documentListChangedEvent.subscribe(
+      (todos)=>{
+        this.todos = todos
+      }
+    )
     console.log(this.user.id)
     const myTodos = this.todoService.getTodoByExecuter(this.user.id)
     // calculating money
@@ -23,6 +33,7 @@ export class UserItemComponent implements OnInit {
         this.myBudget += todo.price
       }
     })
+    console.log(myTodos)
   }
   
     
