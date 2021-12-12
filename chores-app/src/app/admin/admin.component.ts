68d90f3480f30bafce3398816a5b4fb17e114ae4
Tincho228 <martin_quintero521@hotmail.com>
2021-12-11 21:35:51 -0300

    finish all the tables with actions

--- chores-app/src/app/admin/admin.component.ts
+++ chores-app/src/app/admin/admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Todo } from '../todos/todo.model';
+import { TodosService } from '../todos/todos.service';
+import { UserItemService } from '../users/user-item.service';
+import { UserService } from '../users/user.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  todos:Todo[]=[]
+  subscription:Subscription
+  availableTodo:Todo[]=[]
+  completedTodo:Todo[]=[]
+  incompletedTodo:Todo[]=[]
+  waitingTodo:Todo[]=[]
 
-  constructor() { }
+  headers = ["name", "description","executer_id","price","status","accions"]
+  constructor(
+    private todoService:TodosService,
+    private userService:UserService,
+    private userItemService:UserItemService
 
-  ngOnInit(): void {
+  ) { }
+
+  async ngOnInit() {
+    this.todos = await this.todoService.waitedTodos()
+    this.subscription = this.todoService.documentListChangedEvent.subscribe(
+      (todos)=>{
+        this.todos = todos
+      }
+    )
+    //filters
+    this.availableTodo = this.todos.filter(todo=>todo.status==="available")
+    this.completedTodo = this.todos.filter(todo=>todo.status==="completed")
+    this.incompletedTodo = this.todos.filter(todo=>todo.status==="incomplete")
+    this.waitingTodo = this.todos.filter(todo=>todo.status==="waiting")
+    
   }
 
 }
